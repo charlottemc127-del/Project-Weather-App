8d54939c6f8d267ee29d23a1f18ba47341c41c31
charlottemc127-del <charlottemc127@gmail.com>
2025-09-14 12:58:44 +0100

    Search functionality with api

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+function updateWeatherInfo(response) {
+  let tempertureElement = document.querySelector("#weather-app-temperture");
+  tempertureElement.innerHTML = response.data.temperture.current;
+  let cityElement = document.querySelector("#weather-app-city");
+
+  cityElement.innerHTML = response.data.city;
+  tempertureElement.innerHTML = Math.round(temperture);
+}
+
+function searchCity(city) {
+  let apiKey = "04atba3fbfaf6of3a11828de020976f3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Newcastle");
